969a878e92388f063e88cad8cde77dc36940d0f2
Hein Andre Grønnestad <hag@haugnett.no>
2023-08-09 02:51:26 +0200

    Updated example to include new API methods and data, added token caching

--- NissanConnect/NissanConnectLib.Example/Program.cs
+++ NissanConnect/NissanConnectLib.Example/Program.cs
@@ -1,39 +1,63 @@
-﻿namespace NissanConnectLib.Example
+﻿using NissanConnectLib.Api;
+using NissanConnectLib.Models;
+using System.Text.Json;
+
+namespace NissanConnectLib.Example
 {
     internal class Program
     {
+        private const string TOKEN_CACHE_FILE = "token.cache";
+
         static async Task Main(string[] args)
         {
-            // Are we missing arguments?
-            if (args.Length == 0)
-            {
-                Console.WriteLine("Command line arguments are missing. Specify either an existing token or a username and password");
-                return;
-            }
-
             // Instantiate client
             var ncc = new NissanConnectClient(Configuration.Region.EU);
             var loggedIn = false;
 
-            // Log in using an existing token
-            if (args.Length == 1)
+            // Try to use token cache file
+            if (File.Exists(TOKEN_CACHE_FILE))
             {
-                loggedIn = await ncc.LogIn(args[0]);
-                Console.WriteLine($"Logged in using token: {ncc.Token}");
+                var cachedToken = JsonSerializer.Deserialize<OAuthAccessTokenResult>(File.ReadAllText(TOKEN_CACHE_FILE));
+                ncc.AccessToken = cachedToken;
+
+                if (ncc.GetUserId() is null)
+                {
+                    Console.WriteLine("Could not get user ID using cached token, deleting cache file...");
+                    File.Delete(TOKEN_CACHE_FILE);
+                }
+                else
+                {
+                    Console.WriteLine("Cached token is valid!");
+                    loggedIn = true;
+                }
             }
 
-            // Log in using a username and password
-            if (args.Length == 2)
-            {
-                loggedIn = await ncc.LogIn(args[0], args[1]);
-            }
-
-            // Are we logged in?
+            // Log in using username and password
             if (!loggedIn)
             {
-                Console.WriteLine("Login failed!");
-                Console.ReadLine();
-                return;
+                // Are we missing arguments?
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("Command line arguments are missing. Specify username and password");
+                    return;
+                }
+
+                // Log in using a username and password
+                if (args.Length == 2)
+                {
+                    loggedIn = await ncc.LogIn(args[0], args[1]);
+                    if (loggedIn)
+                    {
+                        Console.WriteLine("Logged in using username and password. Writing token to cache file...");
+                        File.WriteAllText(TOKEN_CACHE_FILE, JsonSerializer.Serialize(ncc.AccessToken));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Login failed!");
+                        Console.ReadLine();
+                        return;
+                    }
+                }
             }
 
             // Get the user id
@@ -45,6 +69,7 @@
                 return;
             }
             Console.WriteLine($"Logged in as: {userId}");
+            Console.WriteLine($"Access Token: {ncc.AccessToken?.AccessToken ?? "null"}");
 
             // Get all cars
             var cars = await ncc.GetCars(userId);
@@ -70,14 +95,43 @@
 
                 // Get battery status for car
                 var bs = await ncc.GetBatteryStatus(car.Vin);
-                if (bs?.Attributes == null)
+                if (bs == null)
                 {
                     Console.WriteLine("      Couldn't get battery status!");
                     continue;
                 }
-                Console.WriteLine($"      Battery: {bs.Attributes.BatteryLevel}%");
-                Console.WriteLine($"      Range: {bs.Attributes.RangeHvacOff} km");
-                Console.WriteLine($"      LastUpdateTime: {bs.Attributes.LastUpdateTime}");
+                Console.WriteLine($"   BatteryStatus");
+                Console.WriteLine($"      BatteryLevel: {bs.BatteryLevel}%");
+                Console.WriteLine($"      RangeHvacOff: {bs.RangeHvacOff} km");
+                Console.WriteLine($"      RangeHvacOn: {bs.RangeHvacOn} km");
+                Console.WriteLine($"      LastUpdateTime: {bs.LastUpdateTime}");
+                Console.WriteLine($"      BatteryStatusAge: {bs.BatteryStatusAge}");
+                Console.WriteLine($"      PlugStatus: {bs.PlugStatus}");
+                Console.WriteLine($"      PlugStatusDetail: {bs.PlugStatusDetail}");
+                Console.WriteLine($"      ChargeStatus: {bs.ChargeStatus}");
+                Console.WriteLine($"      ChargePower: {bs.ChargePower}");
+
+                // Get HVAC status for car
+                var hvacs = await ncc.GetHvacStatus(car.Vin);
+                if (hvacs == null)
+                {
+                    Console.WriteLine("      Couldn't get HVAC status!");
+                    continue;
+                }
+                Console.WriteLine($"   HvacStatus");
+                Console.WriteLine($"      SocThreshold: {hvacs.SocThreshold}%");
+                Console.WriteLine($"      LastUpdateTime: {hvacs.LastUpdateTime}");
+                Console.WriteLine($"      HvacStatus: {hvacs.HvacStatus}");
+
+                // Get cockpit status for car
+                var cs = await ncc.GetCockpitStatus(car.Vin);
+                if (cs == null)
+                {
+                    Console.WriteLine("      Couldn't get cockpit status!");
+                    continue;
+                }
+                Console.WriteLine($"   Cockpit");
+                Console.WriteLine($"      TotalMileage: {cs.TotalMileage} km");
             }
         }
     }
